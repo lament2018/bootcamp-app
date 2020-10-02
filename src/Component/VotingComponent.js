f5e80646fbf5f47c6c025d9de29925cea045792d
Linh Do <linhdo@Linhs-MacBook-Pro.local>
2020-10-02 12:08:22 -0500

    adding component

--- src/Component/VotingComponent.js
+++ src/Component/VotingComponent.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faThumbsDown, faThumbsUp } from '@fortawesome/free-solid-svg-icons';
 
 export default function DisplayVote(props){
     return(
         <div>
-            <h2>{props.name} ({props.array.length})</h2>
-           <List array={props.array} function={props.function} />
+              {props.arrayItems.sort(function(a,b){
+                return b.vote - a.vote;
+            }).map((item,index) =>
+            <div id="vote-row" key={index}>
+            <span id="vote-num">{item.vote}</span>
+            <img src={item.imgUrl} className="brand-logo"/>
+            {/* <h3 id="vote-name">{item.name}</h3> */}
+            <div id="voting-btn">
+            <button onClick={()=>props.functionAdd(item)} id="btn-add"><FontAwesomeIcon icon={faThumbsUp} /></button>
+            <button onClick={()=>props.functionMinus(item)} id="btn-remove"><FontAwesomeIcon icon={faThumbsDown} /></button>
+            </div>
         </div>
+        
+        )}
+        </div>
+      
+    
     )
 }
--- src/VotingApp.js
+++ src/VotingApp.js
@@ -1,18 +1,18 @@
 import React from 'react';
 //import images from './img'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faThumbsDown, faThumbsUp } from '@fortawesome/free-solid-svg-icons';
+
+import VotingComponent from './Component/VotingComponent';
 export default function VotingApp(){
     
 
     let defaultItems=[
         {name: 'Starbucks', vote: 0, imgUrl: require("./img/starbucks-226353.png")},
-        {name: 'Lavazza', vote: 10,imgUrl: require("./img/lavazza-bnr.jpg")},
-        {name: 'Red Bay', vote: 10,imgUrl: require("./img/red-bay.jpeg")},
-        {name: 'Cafe Du Monde', vote: 10,imgUrl: require("./img/cafe-du-monde-logo.jpg")},
-        {name: 'Dunkin Donuts', vote: 10,imgUrl: require("./img/dunkin_donut.jpeg")},
-        {name: 'Gloria Jeans', vote: 10,imgUrl: require("./img/Gloria_Jean's_Coffees_logo.jpg")},
-        {name: 'Kickapoo', vote: 10,imgUrl: require("./img/kickapoo.jpg")}
+        {name: 'Lavazza', vote: 30,imgUrl: require("./img/lavazza-bnr.jpg")},
+        {name: 'Red Bay', vote: 5,imgUrl: require("./img/red-bay.jpeg")},
+        {name: 'Cafe Du Monde', vote: 90,imgUrl: require("./img/cafe-du-monde-logo.jpg")},
+        {name: 'Dunkin Donuts', vote: 6,imgUrl: require("./img/dunkin_donut.jpeg")},
+        {name: 'Gloria Jeans', vote: 28,imgUrl: require("./img/Gloria_Jean's_Coffees_logo.jpg")},
+        {name: 'Kickapoo', vote: 59,imgUrl: require("./img/kickapoo.jpg")}
     ]
     const[items,setItems] = React.useState(defaultItems);
 
@@ -34,17 +34,9 @@ export default function VotingApp(){
     return(
         <div id="vote-container">
             <h1>Coffee Brand Rating App</h1>
-            {items.sort(function(a,b){
-                return b.vote - a.vote;
-            }).map(item=><div id="vote-row">
-                <span id="vote-num">{item.vote}</span>
-                <img src={item.imgUrl} className="brand-logo"/>
-                {/* <h3 id="vote-name">{item.name}</h3> */}
-                <div id="voting-btn">
-                <button onClick={()=>addVote(item)} id="btn-add"><FontAwesomeIcon icon={faThumbsUp} /></button>
-                <button onClick={()=>removeVote(item)} id="btn-remove"><FontAwesomeIcon icon={faThumbsDown} /></button>
-                </div>
-            </div>)}
+            <VotingComponent arrayItems = {items}
+                                functionAdd = {addVote}
+                                functionMinus = {removeVote} />
         </div>
     )
      
